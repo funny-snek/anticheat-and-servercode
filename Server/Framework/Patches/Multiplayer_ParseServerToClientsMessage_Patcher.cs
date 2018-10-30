afa381d5070a206ecab2060a394e8991a98cfc15
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2018-10-30 14:06:40 -0400

    rewrite Anti-Cheat Server
    This commit...
    - replaces heavily duplicated code with a list of models;
    - fixes the limit of 30 validated players;
    - fixes some connections, disconnections, or incoming player IDs being ignored if more than one was received within one second;
    - removed the temporary file used to store the latest multiplayer ID.

--- Server/Framework/Patches/Multiplayer_ParseServerToClientsMessage_Patcher.cs
+++ Server/Framework/Patches/Multiplayer_ParseServerToClientsMessage_Patcher.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.IO;
-using StardewValley;
+﻿using StardewValley;
 
 namespace FunnySnek.AntiCheat.Server.Framework.Patches
 {
@@ -15,38 +13,10 @@ namespace FunnySnek.AntiCheat.Server.Framework.Patches
         /*********
         ** Public methods
         *********/
-        public static void Prefix(string message) //not sure how to do this line?
+        public static void Prefix(string message)
         {
-            if (message.Substring(0, 4) == "SCAZ")//currentpass
-            {
-                //save message to txt file
-                try
-                {
-
-                    //Pass the filepath and filename to the StreamWriter Constructor
-                    StreamWriter sw = new StreamWriter("Mods/anticheatviachat/ReceivedMultiplayerID.txt");
-
-                    //Write a line of text
-                    sw.WriteLine(message);
-                    //Close the file
-                    sw.Close();
-                }
-                catch (Exception b)
-                {
-                    Console.WriteLine("Exception: " + b.Message);
-                }
-                finally
-                {
-                    Console.WriteLine("Executing finally block.");
-                }
-                //use this to add message to my list in ModEntry
-                ModEntry.Messages.Add(message);
-                //////////////////////////////////////////
-                // to send messages
-                //Game1.client.sendMessage((byte) 18, myStringMessage)
-
-            }
-
+            // track new messages
+            ModEntry.MessagesReceived.Add(message);
         }
     }
 }
